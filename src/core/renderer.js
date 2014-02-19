6d9717e84a13e36fa1aee8ccc7c7c9fd5a1bd78d
Alex Petersen <theoperatore@gmail.com>
2014-02-19 13:25:49 -0600

    moved functions from renderer to state

--- src/core/renderer.js
+++ src/core/renderer.js
@@ -7,89 +7,30 @@ var rend = {
 	ctx : null,
 	height : 0,
 	width : 0,
-	player : null,
-	enemies : [],
-	npcs : [],
-	interactables : [],
-	level : '1',
-	scenery : {
-		background : null,
-		backdrop : null,
-		foreground : null
-	},
-	state : null,
+	state : null
 };
 
 /******************************************************************************
 
-Must be called first to setup drawing context and dimensions of the canvas
+Must be called first to setup drawing context, canvas dimensions and game state
 
 ******************************************************************************/
-exports.init = function(context, width, height) {
+exports.init = function(context, width, height, state) {
 	rend.ctx = context;
 	rend.width = width;
 	rend.height = height;
-	rend.state = new State('title');
+	rend.state = state || new State('title');
 }
 
 /******************************************************************************
 
-Setup the player with the renderer
+Changes the current game state to the provided state; if the provided state is
+not of the type 'state',  the game stat will not be changed
 
 ******************************************************************************/
-exports.addPlayerToRenderer = function(player) {
-	rend.player = player;
-}
-
-/******************************************************************************
-
-Setup enemies to be drawn; can pass in either a single enemy, or an array
-
-******************************************************************************/
-exports.addEnemyToRenderer = function(enemy) {
-	if (typeof enemy === 'entity') {
-		rend.enemies.push(enemy);
-	}
-
-	else if (enemy.length) {
-		for (var i = 0; i < enemy.length; i++) {
-			rend.enemies.push(enemy[i]);
-		}
-	}
-}
-
-/******************************************************************************
-
-Add NPCs to the renderer; can add single npcs or an array
-
-******************************************************************************/
-exports.addNPCToRenderer = function(npc) {
-	if (typeof npc === 'entity') {
-		rend.npcs.push(npc);
-	}
-
-	else if (npc.length) {
-		for (var i = 0; i < npc.length; i++) {
-			rend.npcs.push(npc[i]);
-		}
-	}
-}
-
-/******************************************************************************
-
-Add interactable to renderer; single or an array
-
-******************************************************************************/
-exports.addInteractable = function(interactable) {
-	if (typeof interactable === 'entity') {
-		rend.interactables.push(interactable);
-	}
-
-	else if (interactable.length) {
-		for (var i = 0; i < interactable.length; i++) {
-			rend.interactables.push(interactable[i]);
-		}
-	}
+exports.changeState = function(newState) {
+	newState = (typeof newState === 'state') ? newState || rend.state;
+	rend.state = newState;
 }
 
 /******************************************************************************
@@ -97,53 +38,57 @@ exports.addInteractable = function(interactable) {
 Draw all setup objects to the screen
 
 ******************************************************************************/
-exports.draw = function(state) {
-
-	//must be told which state to render
+exports.draw = function() {
 
 	if (rend.ctx != null) {
 
-		//clear the canvas
-		rend.ctx.clearRect(0, 0, rend.width, rend.height);
+		if (rend.state != null) {
 
-		//draw backdrop ... parallax?
-		if (rend.scenery.backdrop != null) {
+			//clear the canvas
+			rend.ctx.clearRect(0, 0, rend.width, rend.height);
 
+			//draw backdrop ... parallax?
+			if (rend.state.scenery.backdrop != null) {
+
+			}
+
+			//draw background
+			if (rend.state.scenery.background != null) {
+
+			}
+
+			//draw npcs
+			if (rend.state.npcs.length > 0) {
+
+			}
+
+			//draw interactables ... powerups?
+			if (rend.state.interactables.length > 0) {
+
+			}
+
+			//draw enemies
+			if (rend.state.enemies.length > 0) {
+
+			}
+
+			//draw player including upgrades
+			if (rend.state.player != null) {
+
+			}
+
+			//draw foreground
+			if (rend.state.scenery.foreground != null) {
+
+			}
+
+			//draw hud
+			if (rend.state.hud != null) {
+
+			}
 		}
-
-		//draw background
-		if (rend.scenery.background != null) {
-
-		}
-
-		//draw npcs
-		if (rend.npcs.length > 0) {
-
-		}
-
-		//draw interactables ... powerups?
-		if (rend.interactables.length > 0) {
-
-		}
-
-		//draw enemies
-		if (rend.enemies.length > 0) {
-
-		}
-
-		//draw player including upgrades
-		if (rend.player != null) {
-
-		}
-
-		//draw foreground
-		if (rend.scenery.foreground != null) {
-
-		}
-
-		//draw hud
-		if (rend.hud != null) {
-
+		else {
+			console.log('Game state is not set!');
 		}
 	}
 	else {
--- src/core/state.js
+++ src/core/state.js
@@ -19,5 +19,65 @@ var State = function State(name) {
 	this.hud = null;
 };
 
+/******************************************************************************
+
+Setup the player with this state
+
+******************************************************************************/
+State.prototype.addPlayerToState = function(player) {
+	this.player = player;
+}
+
+/******************************************************************************
+
+Setup enemies to the state; can pass in either a single enemy, or an array
+
+******************************************************************************/
+State.prototype.addEnemyToState = function(enemy) {
+	if (typeof enemy === 'entity') {
+		this..enemies.push(enemy);
+	}
+
+	else if (enemy.length) {
+		for (var i = 0; i < enemy.length; i++) {
+			this..enemies.push(enemy[i]);
+		}
+	}
+}
+
+/******************************************************************************
+
+Add NPCs to this State; can add single npcs or an array
+
+******************************************************************************/
+State.prototype.addNPCToState = function(npc) {
+	if (typeof npc === 'entity') {
+		this..npcs.push(npc);
+	}
+
+	else if (npc.length) {
+		for (var i = 0; i < npc.length; i++) {
+			this..npcs.push(npc[i]);
+		}
+	}
+}
+
+/******************************************************************************
+
+Add interactable to this State; single or an array
+
+******************************************************************************/
+State.prototype.addInteractable = function(interactable) {
+	if (typeof interactable === 'entity') {
+		this.interactables.push(interactable);
+	}
+
+	else if (interactable.length) {
+		for (var i = 0; i < interactable.length; i++) {
+			this..interactables.push(interactable[i]);
+		}
+	}
+}
+
 module.exports = State;
 
