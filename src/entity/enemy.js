62b574a3cf491efc1b4e943ac917a982a36a4b9b
Alex Petersen <theoperatore@gmail.com>
2014-03-11 14:19:49 -0500

    added enemy class

---
+++ src/entity/enemy.js
@@ -0,0 +1,43 @@
+var Entity = require('./entity'),
+    AABB = require('../core/boundingbox');
+
+
+var Enemy = function() {
+
+  //inheritance
+  Entity.call(this);
+
+  //boundig boxes
+  this.aabbs = [];
+}
+
+//inheritance
+Enemy.prototype = Object.create(Entity.prototype);
+
+/******************************************************************************
+
+Add a new aabb to the enemy
+
+******************************************************************************/
+Enemy.prototype.addAABB = function(x, y, width, height) {
+  var box = new AABB(this.pos.x, this.pos.y, x, y, width, height);
+
+  this.aabbs.push(box);
+}
+
+/******************************************************************************
+
+Update this enemy's position and bounding boxes
+
+******************************************************************************/
+Enemy.prototype.update = function(dt) {
+
+  this.updateRungeKutta(dt);
+
+  for (var i = 0; i < this.aabbs.length; i++) {
+
+    this.aabbs[i].updatePos(this.pos.x, this.pos.y);
+
+  }
+
+}
