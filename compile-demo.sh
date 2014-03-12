fe5ce2761a3834c650858a300f465e7cf098c938
Alex Petersen <theoperatore@gmail.com>
2014-03-12 14:19:26 -0500

    modified compile script

--- compile-demo.sh
+++ compile-demo.sh
@@ -2,4 +2,4 @@
 
 browserify src/ph-engine.js | uglifyjs > app.js
 cp -Rfv app.js ~/projects/super-party-horse-blog/demo
-cp -Rfv src/resources/ ~/projects/super-party-horse-blog/demo/resources/
+cp -Rfv src/resources/ ~/projects/super-party-horse-blog/demo/src/resources/
